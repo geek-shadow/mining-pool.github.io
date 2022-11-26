1b9f218c11e48d72ad39edfecf71ce749500cbfc
geek-shadow <93735707+geek-shadow@users.noreply.github.com>
2022-11-26 20:18:54 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Mining Pool - Home</title>
+    <link rel="shortcut icon" href="img/img4.png" type="image/x-icon">
+</head>
+<body>
+    <div class="topBar">
+        <a class="no-txt-a-tag" href="index.php"><button class="btn btn-outline-warning">Home</button></a>
+        <a class="no-txt-a-tag" href="download.php"><button class="btn btn-primary">Download</button></a>
+        <a class="no-txt-a-tag" href="contact.php"><button class="btn btn-warning">Contact</button></a>
+        <a class="no-txt-a-tag" href="how-to.php"><button class="btn btn-warning">How To Use?</button></a>
+
+    </div>
+    <div class="container">
+        <div class="row">
+            <!-- 1st Item -->
+            <div class="col-sm-12 col-md-12 col-lg-6">
+                <div class="paper">
+                    <h1>Mining Pool</h1>
+                    <p>The Best Mining Pool Servers For You. 💰</p>
+                    <ul>
+                        <li>Fastest</li>
+                        <li>Safest</li>
+                        <li>Easiest</li>
+                    </ul>
+                    <br>
+                    <a class="no-txt-a-tag" href="download.php"><button class="btn btn-primary">Download</button></a>
+                </div>
+                
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-6">
+                <div class="paper">
+                    <img class="img-cover" src="img/img1.png">
+                </div>
+            </div>
+
+
+            <!-- 2nd Item -->
+            <div class="col-sm-12 col-md-12 col-lg-6">
+                <div class="paper">
+                    <table class="tbcoins">
+						<tr>
+							<td><img class="imgicon" src="img/coins/btc.png"></td>
+							<td>Bitcoin</td>
+							<td>[BTC]</td>
+						</tr>
+
+						<tr>
+							<td><img class="imgicon" src="img/coins/eth.png"></td>
+							<td>Ethereum</td>
+							<td>[ETH]</td>
+						</tr>
+
+						<tr>
+							<td><img class="imgicon" src="img/coins/etc.png"></td>
+							<td>Ethereum Classic</td>
+							<td>[ETC]</td>
+						</tr>
+
+						<tr>
+							<td><img class="imgicon" src="img/coins/ltc.png"></td>
+							<td>Litecoint</td>
+							<td>[LTC]</td>
+						</tr>
+
+						<tr>
+							<td><img class="imgicon" src="img/coins/xmr.png"></td>
+							<td>Monero</td>
+							<td>[XMR]</td>
+						</tr>
+
+						<tr>
+							<td><img class="imgicon" src="img/coins/doge.png"></td>
+							<td>Dogecoin</td>
+							<td>[DOGE]</td>
+						</tr>
+
+						<tr>
+							<td><img class="imgicon" src="img/coins/zec.png"></td>
+							<td>Zcash</td>
+							<td>[ZEC]</td>
+						</tr>
+
+						<tr>
+							<td><img class="imgicon" src="img/coins/trx.png"></td>
+							<td>TRON</td>
+							<td>[TRX]</td>
+						</tr>
+						
+					</table>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-6">
+                <div class="paper">
+                    <img class="img-cover" src="img/img2.png">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
